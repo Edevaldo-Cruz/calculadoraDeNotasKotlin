56f698bd54572c04995ac45473295ef47a787998
Edevaldo-Cruz <77896103+Edevaldo-Cruz@users.noreply.github.com>
2022-02-25 10:21:51 -0300

    Initial commit

---
+++ app/src/main/java/com/example/calculadoradenotas/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.calculadoradenotas
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btCalcular = calcular
+        val resultado = resultado
+
+        btCalcular.setOnClickListener {
+
+            val nota1 : Int = Integer.parseInt(nota1.text.toString())
+            val nota2 : Int = Integer.parseInt(nota2.text.toString())
+            val media : Int = (nota1 + nota2)/2
+            val faltas : Int = Integer.parseInt(faltas.text.toString())
+
+            if(media >=6 && faltas <=10) {
+                resultado.setText("aluno foi Aprovado" +
+                        "\n" + "Nota Final:" + media +
+                        "\n" + "Faltas:" + faltas)
+                resultado.setTextColor(Color.GREEN)
+            } else {
+                resultado.setText("aluno foi Reprovado" +
+                        "\n" + "Nota Final:" + media +
+                        "\n" + "Faltas:" + faltas)
+                resultado.setTextColor(Color.RED)
+            }
+
+        }
+    }
+}
